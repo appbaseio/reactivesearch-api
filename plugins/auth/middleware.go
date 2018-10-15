919b9269ee387e633061f177e530daaefff073c8
utsavoza <utsavoza96@gmail.com>
2018-10-15 18:41:33 +0530

    Remove user and permission from cache before patch

--- plugins/auth/middleware.go
+++ plugins/auth/middleware.go
@@ -21,9 +21,6 @@ func (a *Auth) BasicAuth(h http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 
-		masterUserId := os.Getenv("USER_ID")
-		masterPassword := os.Getenv("PASSWORD")
-
 		ctxACL := r.Context().Value(acl.CtxKey)
 		if ctxACL == nil {
 			log.Printf("%s: request must be classified before it is authenticated", logTag)
@@ -31,38 +28,20 @@ func (a *Auth) BasicAuth(h http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 
-		obj, ok := ctxACL.(*acl.ACL)
+		reqACL, ok := ctxACL.(*acl.ACL)
 		if !ok {
-			log.Printf("%s: unable to cast context acl %v to type *acl.ACL", logTag, obj)
+			log.Printf("%s: unable to cast context acl %v to type *acl.ACL", logTag, ctxACL)
 			util.WriteBackMessage(w, "Internal server error", http.StatusInternalServerError)
 			return
 		}
 
-		if obj.IsFromES() {
+		if reqACL.IsFromES() {
 			var err error
 			var p *permission.Permission
 
-			// TODO: temporary entry point?
-			if userId == masterUserId && password == masterPassword {
-				p, err = a.createAdminPermission(userId)
-				if err != nil {
-					msg := fmt.Sprintf(`unable to create admin permission for "creator"="%s"`,
-						userId)
-					log.Printf("%s: %s: %v", logTag, msg, err)
-					util.WriteBackError(w, msg, http.StatusInternalServerError)
-					return
-				}
-				ctx := r.Context()
-				ctx = context.WithValue(ctx, permission.CtxKey, p)
-				r = r.WithContext(ctx)
-				//log.Printf("[auth]: took %fs", time.Since(start).Seconds())
-				h(w, r)
-				return
-			}
-
 			// check in the cache
-			//p, ok := a.cachedPermission(userId)
-			//if !ok {
+			p, ok := a.cachedPermission(userId)
+			if !ok {
 				p, err = a.es.getPermission(userId)
 				if err != nil {
 					msg := fmt.Sprintf(`Unable to fetch permission with "creator"="%s"`, userId)
@@ -72,7 +51,7 @@ func (a *Auth) BasicAuth(h http.HandlerFunc) http.HandlerFunc {
 				}
 				// store in the cache
 				a.cachePermission(userId, p)
-			//}
+			}
 
 			if password != p.Password {
 				util.WriteBackMessage(w, "Incorrect credentials", http.StatusUnauthorized)
@@ -86,26 +65,35 @@ func (a *Auth) BasicAuth(h http.HandlerFunc) http.HandlerFunc {
 			var err error
 			var u *user.User
 
-			// TODO: temporary entry point?
-			if userId == masterUserId && password == masterPassword {
-				u, err = a.createAdminUser(userId, password)
-				if err != nil {
-					msg := fmt.Sprintf(`unable to create admin user for "user_id"="%s"`, userId)
-					log.Printf("%s: %s: %v", logTag, msg, err)
-					util.WriteBackError(w, msg, http.StatusInternalServerError)
-					return
+			// if we are patching a user or a permission, we must clear their
+			// respective objects from the cache, otherwise the changes won't be
+			// reflected the next time user tries to get the user or permission object.
+			if r.Method == http.MethodPatch {
+				switch *reqACL {
+				case acl.User:
+					a.removeUserFromCache(userId)
+				case acl.Permission:
+					a.removePermissionFromCache(userId)
 				}
+			}
+
+			u, err = a.isMaster()
+			if err != nil {
+				log.Printf("%s: %v", logTag, err)
+				util.WriteBackMessage(w, "Internal server error", http.StatusInternalServerError)
+				return
+			}
+			if u != nil {
 				ctx := r.Context()
 				ctx = context.WithValue(ctx, user.CtxKey, u)
 				r = r.WithContext(ctx)
-				//log.Printf("[auth]: took %fs", time.Since(start).Seconds())
 				h(w, r)
 				return
 			}
 
 			// check in the cache
-			//u, ok := a.cachedUser(userId)
-			//if !ok {
+			u, ok := a.cachedUser(userId)
+			if !ok {
 				u, err = a.es.getUser(userId)
 				if err != nil {
 					msg := fmt.Sprintf(`Unable to fetch user with "user_id"="%s"`, userId)
@@ -115,7 +103,8 @@ func (a *Auth) BasicAuth(h http.HandlerFunc) http.HandlerFunc {
 				}
 				// store in the cache
 				a.cacheUser(userId, u)
-			//}
+			}
+
 			if password != u.Password {
 				util.WriteBackMessage(w, "Incorrect credentials", http.StatusUnauthorized)
 				return
@@ -147,6 +136,21 @@ func (a *Auth) cacheUser(userId string, u *user.User) {
 	a.usersCache[userId] = u
 }
 
+func (a *Auth) removeUserFromCache(userId string) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	delete(a.usersCache, userId)
+}
+
+func (a *Auth) createAdminUser(userId, password string) (*user.User, error) {
+	u := user.NewAdmin(userId, password)
+	ok, err := a.es.putUser(*u)
+	if !ok || err != nil {
+		return nil, err
+	}
+	return u, nil
+}
+
 func (a *Auth) cachedPermission(username string) (*permission.Permission, bool) {
 	a.mu.Lock()
 	defer a.mu.Unlock()
@@ -164,6 +168,12 @@ func (a *Auth) cachePermission(username string, p *permission.Permission) {
 	a.permissionsCache[username] = p
 }
 
+func (a *Auth) removePermissionFromCache(username string) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	delete(a.permissionsCache, username)
+}
+
 func (a *Auth) createAdminPermission(creator string) (*permission.Permission, error) {
 	p := permission.NewAdmin(creator)
 	ok, err := a.es.putPermission(*p)
@@ -174,11 +184,21 @@ func (a *Auth) createAdminPermission(creator string) (*permission.Permission, er
 	return p, nil
 }
 
-func (a *Auth) createAdminUser(userId, password string) (*user.User, error) {
-	u := user.NewAdmin(userId, password)
-	ok, err := a.es.putUser(*u)
-	if !ok || err != nil {
-		return nil, err
+func (a *Auth) isMaster() (*user.User, error) {
+	masterUser, masterPassword := os.Getenv("USER_ID"), os.Getenv("PASSWORD")
+	if masterUser == "" && masterPassword == "" {
+		return nil, nil
 	}
-	return u, nil
+
+	master, err := a.es.getUser(masterUser)
+	if err != nil {
+		log.Printf("%s: master user doesn't exists, creating one... : %v", logTag, err)
+		master = user.NewAdmin(masterUser, masterPassword)
+		ok, err := a.es.putUser(*master)
+		if !ok || err != nil {
+			return nil, fmt.Errorf("%s: unable to create master user: %v", logTag, err)
+		}
+	}
+
+	return master, nil
 }
