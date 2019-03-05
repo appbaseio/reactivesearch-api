81ad877cf91fe8341e53441ab7eed113550c2efc
raazcrzy <raaz.crzy@gmail.com>
2019-03-05 18:14:07 +0530

    bugfix admin user creation

--- plugins/users/handlers.go
+++ plugins/users/handlers.go
@@ -109,12 +109,23 @@ func (u *Users) postUser() http.HandlerFunc {
 			util.WriteBackError(w, `user "password" shouldn't be empty`, http.StatusBadRequest)
 			return
 		}
-		newUser, err := user.New(userBody.Username, userBody.Password, opts...)
-		if err != nil {
-			msg := fmt.Sprintf("an error occurred while creating user: %v", err)
-			log.Printf("%s: %s: %v", logTag, msg, err)
-			util.WriteBackError(w, msg, http.StatusBadRequest)
-			return
+		var newUser *user.User
+		if *userBody.IsAdmin {
+			newUser, err = user.NewAdmin(userBody.Username, userBody.Password, opts...)
+			if err != nil {
+				msg := fmt.Sprintf("an error occurred while creating user: %v", err)
+				log.Printf("%s: %s: %v", logTag, msg, err)
+				util.WriteBackError(w, msg, http.StatusBadRequest)
+				return
+			}
+		} else {
+			newUser, err = user.New(userBody.Username, userBody.Password, opts...)
+			if err != nil {
+				msg := fmt.Sprintf("an error occurred while creating user: %v", err)
+				log.Printf("%s: %s: %v", logTag, msg, err)
+				util.WriteBackError(w, msg, http.StatusBadRequest)
+				return
+			}
 		}
 
 		rawUser, err := json.Marshal(*newUser)
