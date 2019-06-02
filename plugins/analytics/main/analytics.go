0f368143b3fece602898a4f3f8e78a5f96bfb687
BSRK Aditya <bsrk@sixb.in>
2019-06-02 09:50:37 +0530

    Remove analytics & rules plugins

--- plugins/analytics/main/analytics.go
+++
@@ -1,6 +0,0 @@
-package main
-
-import "github.com/appbaseio-confidential/arc/plugins/analytics"
-import "github.com/appbaseio-confidential/arc/plugins"
-
-var PluginInstance plugins.Plugin = analytics.Instance()
--- plugins/rules/main/rules.go
+++
@@ -1,6 +0,0 @@
-package main
-
-import "github.com/appbaseio-confidential/arc/plugins/rules"
-import "github.com/appbaseio-confidential/arc/plugins"
-
-var PluginInstance plugins.Plugin = rules.Instance()
