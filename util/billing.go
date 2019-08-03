c70a705cb48f121bee2ec95c07736c5fbc26ba8e
kuldeep <kuldepsaxena155@gmail.com>
2019-08-04 02:03:24 +0530

    fix: update URL

--- util/billing.go
+++ util/billing.go
@@ -16,8 +16,7 @@ import (
 
 var TimeValidity int64
 var MAX_ALLOWED_TIME int64 = 24 // in hrs
-// var ACC_API = "https://accapi.appbase.io/"
-var ACC_API = "http://localhost:3000/"
+var ACC_API = "https://accapi.appbase.io/"
 
 type ArcUsage struct {
 	ArcID          string `json:"arc_id"`
