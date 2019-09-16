d40591bdf19264222f70af15e9325e8cb10af3cf
Jeet Parekh <jeetparekh96@gmail.com>
2019-09-16 22:45:30 +0530

    plugins/logs/dao_test.go - fixed TestGetRawLogs

--- plugins/logs/dao_test.go
+++ plugins/logs/dao_test.go
@@ -146,7 +146,7 @@ var getRawLogsTest = []struct {
 			Method:   "POST",
 			Path:     "/test1/_search",
 			Body:     `{"from":0,"query":{"bool":{"filter":{"match_all":{}}}},"size":10,"sort":[{"timestamp":{"order":"desc"}}]}`,
-			Response: `{"took":90,"timed_out":false,"_shards":{"total":5,"successful":5,"skipped":0,"failed":0},"hits":{"total":2,"max_score":1.0,"hits":[{"_index":"test","_type":"_doc","_id":"1","_score":1.0,"_source":{"counter":5,"tags":["red","blue","blue","blue","blue","blue"]}},{"_index":"test","_type":"_doc","_id":"3","_score":1.0,"_source":{ "field1" : "value3" }}]}}`,
+			Response: `{"took":90,"timed_out":false,"_shards":{"total":5,"successful":5,"skipped":0,"failed":0},"hits":{"total":{"value": 2, "relation": "eq"},"max_score":1.0,"hits":[{"_index":"test","_type":"_doc","_id":"1","_score":1.0,"_source":{"counter":5,"tags":["red","blue","blue","blue","blue","blue"]}},{"_index":"test","_type":"_doc","_id":"3","_score":1.0,"_source":{ "field1" : "value3" }}]}}`,
 		},
 		"test1",
 		"0",
