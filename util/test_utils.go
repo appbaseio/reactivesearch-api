d7429630aa58c7d76455fed1748fa5a12e47fbcc
bietkul <kuldepsaxena155@gmail.com>
2019-12-19 13:00:41 +0530

    feat: common util to build arc

--- util/test_utils.go
+++ util/test_utils.go
@@ -6,10 +6,50 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os/exec"
+	"strconv"
+	"syscall"
+	"time"
 )
 
 // TestURL for arc
 var TestURL = "http://foo:bar@localhost:8000"
+
+type BuildArc struct {
+	cmd                 *exec.Cmd
+	Tier                Plan
+	FeatureCustomEvents bool
+	FeatureSuggestions  bool
+}
+
+func StartArc(b *BuildArc) BuildArc {
+	ldFlags := "export TEST_TIER=" + b.Tier.String() + " TEST_FEATURE_CUSTOM_EVENTS=" + strconv.FormatBool(b.FeatureCustomEvents) + " TEST_FEATURE_SUGGESTIONS=" + strconv.FormatBool(b.FeatureSuggestions) + ";"
+	makeCmd := exec.Command("/bin/sh", "-c", "cd ..; cd ..;"+ldFlags+"make clean; make;")
+	err := makeCmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	buildCmd := exec.Command("/bin/sh", "-c", "cd ..; cd ..; ./build/arc --env=config/manual.env;")
+	buildCmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	err2 := buildCmd.Start()
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	b.cmd = buildCmd
+	return *b
+}
+
+func (b *BuildArc) start() {
+	b.cmd.Start()
+	time.Sleep(time.Duration(20) * time.Second)
+}
+
+func (b *BuildArc) close() {
+	err := syscall.Kill(-b.cmd.Process.Pid, syscall.SIGKILL)
+	if err != nil {
+		log.Fatal("Unable to kill process", err)
+	}
+}
 
 func StructToMap(response interface{}) interface{} {
 	var mockMap map[string]interface{}
