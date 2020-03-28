9db48b93fb9d6acee842f29c5929be0d20aebb09
BSRK Aditya <bsrk@sixb.in>
2020-03-28 10:35:59 +0530

    Remove FuzzinessAnalysis from IsFromRS

--- model/category/category.go
+++ model/category/category.go
@@ -172,8 +172,7 @@ func (c Category) IsFromES() bool {
 
 // IsFromRS checks whether the category is of the reactivesearch category.
 func (c Category) IsFromRS() bool {
-	return c == ReactiveSearch ||
-	       c == FuzzinessAnalysis
+	return c == ReactiveSearch
 }
 
 // HasACL checks whether the given acl is a value in the category categories.
