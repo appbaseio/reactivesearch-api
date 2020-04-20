ef5e48752b57eecf29e861f4e595511d227c6ab7
Kuldeep Saxena <kuldepsaxena155@gmail.com>
2020-04-21 00:59:37 +0530

    fix: index filtering query for analytics (#99)
    * fix: index filtering query for analytics
    
    * fix: use term query instead of match

--- util/shared.go
+++ util/shared.go
@@ -15,7 +15,7 @@ func GetIndexFilterQueryEs6(query *es6.BoolQuery, indices ...string) *es6.BoolQu
 			query := es6.NewTermQuery("indices.keyword", index)
 			indexQueries = append(indexQueries, query)
 		}
-		query = query.Should(indexQueries...)
+		query = query.Must(es6.NewBoolQuery().Should(indexQueries...))
 	}
 	return query
 }
@@ -28,7 +28,7 @@ func GetIndexFilterQueryEs7(query *es7.BoolQuery, indices ...string) *es7.BoolQu
 			query := es7.NewTermQuery("indices.keyword", index)
 			indexQueries = append(indexQueries, query)
 		}
-		query = query.Should(indexQueries...)
+		query = query.Must(es7.NewBoolQuery().Should(indexQueries...))
 	}
 	return query
 }
