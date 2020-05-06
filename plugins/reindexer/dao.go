0d6860629bec375ea7d55845a312502dda8d71e6
anuj <anujshah584@gmail.com>
2020-05-06 12:31:20 +0530

    fix: use operations enum for reindex

--- plugins/reindexer/dao.go
+++ plugins/reindexer/dao.go
@@ -15,7 +15,7 @@ import (
 	es7 "github.com/olivere/elastic/v7"
 )
 
-func postReIndex(ctx context.Context, sourceIndex, newIndexName string, delOldIndex bool) error {
+func postReIndex(ctx context.Context, sourceIndex, newIndexName string, operation ReIndexOperation) error {
 	// Fetch all the aliases of old index
 	alias, err := aliasesOf(ctx, sourceIndex)
 
@@ -31,7 +31,7 @@ func postReIndex(ctx context.Context, sourceIndex, newIndexName string, delOldIn
 	}
 
 	// Delete old index
-	if delOldIndex {
+	if operation == ReIndexWithDelete {
 		err = deleteIndex(ctx, sourceIndex)
 		if err != nil {
 			return errors.New(`error deleting source index ` + sourceIndex + "\n" + err.Error())
@@ -111,8 +111,11 @@ func reindex(ctx context.Context, sourceIndex string, config *reindexConfig, wai
 		body["settings"] = config.Settings
 	}
 	newIndexName := destinationIndex
-	isClone := destinationIndex != ""
-	if !isClone {
+	operation := ReIndexWithDelete
+	if destinationIndex != "" {
+		operation = ReindexWithClone
+	}
+	if operation == ReIndexWithDelete {
 		newIndexName, err = reindexedName(sourceIndex)
 	}
 
@@ -155,8 +158,8 @@ func reindex(ctx context.Context, sourceIndex string, config *reindexConfig, wai
 			return nil, err
 		}
 
-		if !isClone {
-			err = postReIndex(ctx, sourceIndex, newIndexName, isClone)
+		if operation == ReIndexWithDelete {
+			err = postReIndex(ctx, sourceIndex, newIndexName, ReIndexWithDelete)
 			if err != nil {
 				return nil, err
 			}
@@ -172,7 +175,7 @@ func reindex(ctx context.Context, sourceIndex string, config *reindexConfig, wai
 	}
 	taskID := response.TaskId
 
-	go asyncReIndex(taskID, sourceIndex, newIndexName, isClone)
+	go asyncReIndex(taskID, sourceIndex, newIndexName, operation)
 
 	// Get the reindex task by ID
 	task, err := util.GetClient7().TasksGetTask().TaskId(taskID).Do(context.Background())
@@ -431,7 +434,7 @@ func isTaskCompleted(ctx context.Context, taskID string) (bool, error) {
 
 // go routine to track async re-indexing process for a given source and destination index.
 // it checks every 30s if task is completed or not.
-func asyncReIndex(taskID, source, destination string, isClone bool) {
+func asyncReIndex(taskID, source, destination string, operation ReIndexOperation) {
 	SetCurrentProcess(taskID, source, destination)
 	isCompleted := make(chan bool, 1)
 	ticker := time.Tick(30 * time.Second)
@@ -449,7 +452,7 @@ func asyncReIndex(taskID, source, destination string, isClone bool) {
 			log.Println(logTag, taskID+" task completed successfully")
 			// remove process from current cache
 			RemoveCurrentProcess(taskID)
-			err := postReIndex(ctx, source, destination, isClone)
+			err := postReIndex(ctx, source, destination, operation)
 			if err != nil {
 				log.Errorln(logTag, " post re-indexing error: ", err)
 			}
