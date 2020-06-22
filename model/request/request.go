b3daab77c70057a16eaa878906f438a4e3a7edb6
bietkul <kuldepsaxena155@gmail.com>
2020-06-22 17:20:39 +0530

    feat: add request context

---
+++ model/request/request.go
@@ -0,0 +1,21 @@
+package request
+
+import (
+	"context"
+)
+
+type contextKey string
+
+// CtxKey is a key against which api request will get stored in the context.
+const CtxKey = contextKey("request")
+
+// NewContext returns a new context with the given request body.
+func NewContext(ctx context.Context, request interface{}) context.Context {
+	return context.WithValue(ctx, CtxKey, request)
+}
+
+// FromContext retrieves the api request body stored against the querytranslate.ctxKey from the context.
+func FromContext(ctx context.Context) (*interface{}, error) {
+	ctxRequest := ctx.Value(CtxKey)
+	return &ctxRequest, nil
+}
