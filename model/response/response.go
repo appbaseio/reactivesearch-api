ba9892762fdf1238723680dd1285dc0aecf77081
bietkul <kuldepsaxena155@gmail.com>
2020-06-24 18:43:29 +0530

    fix: use sync map

--- model/response/response.go
+++ model/response/response.go
@@ -7,29 +7,27 @@ var CurrentResponseProcessMutex = sync.RWMutex{}
 
 // Response represents the cached API response for a request
 // Key is the unique ID for each request
-var Response = make(map[string]map[string]interface{})
+var Response = sync.Map{}
 
 // GetResponse returns the response by request ID
 func GetResponse(requestID string) map[string]interface{} {
-	CurrentResponseProcessMutex.Lock()
-	defer CurrentResponseProcessMutex.Unlock()
-	response, ok := Response[requestID]
+	response, ok := Response.Load(requestID)
 	if !ok {
 		return nil
 	}
-	return response
+	responseAsMap, ok := response.(map[string]interface{})
+	if !ok {
+		return nil
+	}
+	return responseAsMap
 }
 
 // SaveResponse returns the response by request ID
 func SaveResponse(requestID string, response map[string]interface{}) {
-	CurrentResponseProcessMutex.Lock()
-	defer CurrentResponseProcessMutex.Unlock()
-	Response[requestID] = response
+	Response.Store(requestID, response)
 }
 
 // ClearResponse clears the cache for a particular request ID
 func ClearResponse(requestID string) {
-	CurrentResponseProcessMutex.Lock()
-	defer CurrentResponseProcessMutex.Unlock()
-	delete(Response, requestID)
+	Response.Delete(requestID)
 }
