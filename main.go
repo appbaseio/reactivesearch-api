b1efe34b87053332f0fb9c92a237b92633888e4d
bietkul <kuldepsaxena155@gmail.com>
2021-03-12 17:55:35 +0530

    change cache plugin order

--- main.go
+++ main.go
@@ -225,7 +225,7 @@ func main() {
 	util.NewClient()
 	util.SetDefaultIndexTemplate()
 	// map of specific plugins
-	sequencedPlugins := []string{"searchrelevancy.so", "rules.so", "functions.so", "analytics.so", "suggestions.so", "cache.so"}
+	sequencedPlugins := []string{"cache.so", "searchrelevancy.so", "rules.so", "functions.so", "analytics.so", "suggestions.so"}
 	sequencedPluginsByPath := make(map[string]string)
 
 	var elasticSearchPath, reactiveSearchPath string
