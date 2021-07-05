f0f1f1eb7ab9cac7f18c0ea807ac225ea68fd33f
bietkul <kuldepsaxena155@gmail.com>
2021-07-05 18:55:30 +0530

    fix: minor fixes

--- main.go
+++ main.go
@@ -269,7 +269,7 @@ func main() {
 	util.SetDefaultIndexTemplate()
 	util.SetSystemIndexTemplate()
 	// map of specific plugins
-	sequencedPlugins := []string{"cache.so", "searchrelevancy.so", "rules.so", "functions.so", "analytics.so", "suggestions.so", "applycache"}
+	sequencedPlugins := []string{"cache.so", "searchrelevancy.so", "rules.so", "functions.so", "analytics.so", "suggestions.so", "applycache.so"}
 	sequencedPluginsByPath := make(map[string]string)
 
 	var elasticSearchPath, reactiveSearchPath string
