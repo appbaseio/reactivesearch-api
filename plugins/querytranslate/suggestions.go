a078b17ff1093aff449a060bc912962481dd6c7e
bietkul <kuldepsaxena155@gmail.com>
2021-10-13 01:33:13 +0530

    fix: remove punctuation

--- plugins/querytranslate/suggestions.go
+++ plugins/querytranslate/suggestions.go
@@ -187,7 +187,7 @@ func populateSuggestionsList(
 			}
 		}
 		suggestion := SuggestionHIT{
-			Value:    getTextFromHTML(suggestionsInfo.value),
+			Value:    strip(getTextFromHTML(suggestionsInfo.value)),
 			Label:    suggestionsInfo.value,
 			URL:      &url,
 			Type:     Index,
@@ -523,6 +523,21 @@ func parseHits(hits []ESDoc) []ESDoc {
 		results = append(results, highlightResults(hit))
 	}
 	return results
+}
+
+// Removes the punctuation from a string
+func strip(s string) string {
+	var result strings.Builder
+	for i := 0; i < len(s); i++ {
+		b := s[i]
+		if ('a' <= b && b <= 'z') ||
+			('A' <= b && b <= 'Z') ||
+			('0' <= b && b <= '9') ||
+			b == ' ' {
+			result.WriteByte(b)
+		}
+	}
+	return result.String()
 }
 
 // Util method to extract the fields from elasticsearch source object
