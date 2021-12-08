9755223410f4d3a443c8359184ae64bfd17f0a9f
Deepjyoti Barman <deep.barman30@gmail.com>
2021-12-08 16:42:15 +0530

    Add a function to get index mapping details from ElasticSearch
    Add a function that gets all index details for the passed index directly
    from ElasticSearch using a get call. This is necessary because the
    olivere/elastic provided index getter method hits the `<target>/_mapping
    /_all` endpoint instead of the `<target>/_mapping` endpoint which breaks
    support for ElasticSearch v8.
    
    Addition of this function will help get rid of such errors until support
    for ElasticSearch v8 is added in the olivere/elastic client.

--- util/migration.go
+++ util/migration.go
@@ -1,8 +1,13 @@
 package util
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/appbaseio/reactivesearch-api/util"
+	log "github.com/sirupsen/logrus"
 )
 
 type Error struct {
@@ -33,12 +38,44 @@ func AddMigrationScript(migration Migration) {
 	migrationScripts = append(migrationScripts, migration)
 }
 
+// Handle unstrctured JSON data from the mapping endpoint
+type IndexMappingResponse map[string]interface{}
+
 // Fetch the index mapping manually using the following function
 // Make the request directly and return the response accordingly.
-func GetIndexMapping(indexName string) (resp *http.Response, err error) {
+// We will extract the unstructured JSON data from the endpoint
+// and parse it to a map so that it can be directly used.
+//
+// On error, an empty data body will be returned along with the
+// error itself.
+//
+// Errors will be returned accordingly and verbosed if the error
+// occurs while extracting the JSON data. There will be no verbose
+// if the error occurs while hitting the endpoint. Those errors are
+// expected to be handled by the calling function.
+func GetIndexMapping(indexName string) (resp IndexMappingResponse, err error) {
 	// Keep a constant variable to store the URL
-	MappingBaseURL := "/%s/_mapping"
+	MappingBaseURL := util.GetESURL() + "/%s/_mapping"
+
+	// Declare the mapping response variable
+	var data IndexMappingResponse
 
 	response, err := http.Get(fmt.Sprintf(MappingBaseURL, indexName))
-	return response, err
+
+	if err != nil {
+		return data, err
+	}
+
+	defer response.Body.Close()
+
+	// Read the body into bytes and try to unmarshall
+	// into JSON data.
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Errorln("error while reading JSON data: ", err)
+		return data, err
+	}
+
+	json.Unmarshal(body, &data)
+	return data, err
 }
