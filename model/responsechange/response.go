c05583e1a90a65ca155ba40efab9cd225f2c54ab
Deepjyoti Barman <deep.barman30@gmail.com>
2022-01-03 11:26:11 +0530

    Add similar context code to store the response changes as well

---
+++ model/responsechange/response.go
@@ -0,0 +1,32 @@
+package responsechange
+
+import (
+	"context"
+
+	"github.com/appbaseio/reactivesearch-api/errors"
+	"github.com/appbaseio/reactivesearch-api/plugins/logs"
+)
+
+type contextKey string
+
+// CtxKey is a key against which api request will get stored in the context.
+const CtxKey = contextKey("response-changes")
+
+// NewContext returns a context with the passed value stored against the
+// context key.
+func NewContext(ctx context.Context, request []logs.Difference) context.Context {
+	return context.WithValue(ctx, CtxKey, request)
+}
+
+// FromContext retrieves the array of request changes saved in the context.
+func FromContext(ctx context.Context) (*[]logs.Difference, error) {
+	ctxRequest := ctx.Value(CtxKey)
+	if ctxRequest == nil {
+		return nil, errors.NewNotFoundInContextError("Response Changes")
+	}
+	changes, ok := ctxRequest.([]logs.Difference)
+	if !ok {
+		return nil, errors.NewInvalidCastError("ctxRequest", "Response Changes")
+	}
+	return &changes, nil
+}
