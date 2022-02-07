c22b45c69268229e4b03b3327e14ec0ab8f0d319
bietkul <kuldepsaxena155@gmail.com>
2022-02-07 22:45:30 +0530

    chore: refactor RSAPI parsing logic

--- plugins/querytranslate/handlers.go
+++ plugins/querytranslate/handlers.go
@@ -3,6 +3,7 @@ package querytranslate
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -58,197 +59,10 @@ func (r *QueryTranslate) search() http.HandlerFunc {
 			return
 		}
 
-		queryIds := GetQueryIds(*rsAPIRequest)
-
-		rsResponse := []byte(`{}`)
-
-		took, valueType1, _, err := jsonparser.Get(httpRes.Body, "took")
-		// ignore not exist error
-		if err != nil && valueType1 != jsonparser.NotExist {
-			log.Errorln(logTag, ":", err)
-			util.WriteBackError(w, "can't parse took key from response", http.StatusInternalServerError)
-			return
-		}
-		// Set the `settings` key to response
-		rsResponseWithTook, err := jsonparser.Set(rsResponse, []byte(fmt.Sprintf(`{ "took": %s }`, string(took))), "settings")
+		rsResponse, err := TransformESResponse(httpRes.Body, rsAPIRequest)
 		if err != nil {
-			log.Errorln(logTag, ":", err)
-			util.WriteBackError(w, "can't add settings key to response", http.StatusInternalServerError)
+			util.WriteBackError(w, err.Error(), http.StatusInternalServerError)
 			return
-		}
-		// Assign updated json to actual response
-		rsResponse = rsResponseWithTook
-
-		responseError, valueType2, _, err := jsonparser.Get(httpRes.Body, "error")
-		// ignore not exist error
-		if err != nil && valueType2 != jsonparser.NotExist {
-			log.Errorln(logTag, ":", err)
-			util.WriteBackError(w, "can't parse error key from response", http.StatusInternalServerError)
-			return
-		} else if responseError != nil {
-			// Set the `error` key to response
-			rsResponseWithError, err := jsonparser.Set(rsResponse, responseError, "error")
-			if err != nil {
-				log.Errorln(logTag, ":", err)
-				util.WriteBackError(w, "can't add error key to response", http.StatusInternalServerError)
-				return
-			}
-			// Assign updated json to actual response
-			rsResponse = rsResponseWithError
-		}
-
-		// Read `responses` value from the response body
-		responses, valueType3, _, err4 := jsonparser.Get(httpRes.Body, "responses")
-		// ignore not exist error
-		if err4 != nil && valueType3 != jsonparser.NotExist {
-			log.Errorln(logTag, ":", err4)
-			util.WriteBackError(w, "can't parse responses key from response", http.StatusInternalServerError)
-			return
-		}
-
-		if responses != nil {
-			index := 0
-			// Set `responses` by query ID
-			jsonparser.ArrayEach(responses, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-				queryID := queryIds[index]
-				var isSuggestionRequest bool
-				var suggestions = make([]SuggestionHIT, 0)
-				// parse suggestions if query is of type `suggestion`
-				for _, query := range rsAPIRequest.Query {
-					if *query.ID == queryID && query.Type == Suggestion {
-						isSuggestionRequest = true
-						// Index suggestions are not meant for empty query
-						if query.Value != nil {
-							valueAsString, ok := (*query.Value).(string)
-							if ok {
-								var normalizedDataFields = []string{}
-								normalizedFields := NormalizedDataFields(query.DataField, query.FieldWeights)
-								for _, dataField := range normalizedFields {
-									normalizedDataFields = append(normalizedDataFields, dataField.Field)
-								}
-								suggestionsConfig := SuggestionsConfig{
-									// Fields to extract suggestions
-									DataFields: normalizedDataFields,
-									// Query value
-									Value:                       valueAsString,
-									ShowDistinctSuggestions:     query.ShowDistinctSuggestions,
-									EnablePredictiveSuggestions: query.EnablePredictiveSuggestions,
-									MaxPredictedWords:           query.MaxPredictedWords,
-									EnableSynonyms:              query.EnableSynonyms,
-									ApplyStopwords:              query.ApplyStopwords,
-									Stopwords:                   query.Stopwords,
-									URLField:                    query.URLField,
-									CategoryField:               query.CategoryField,
-									HighlightField:              query.HighlightField,
-									HighlightConfig:             query.HighlightConfig,
-									Language:                    query.SearchLanguage,
-								}
-
-								var rawHits []ESDoc
-								hits, dataType, _, err1 := jsonparser.Get(value, "hits", "hits")
-								if dataType == jsonparser.NotExist {
-									// write raw response
-									rsResponseWithSearchResponse, err := jsonparser.Set(rsResponse, value, queryID)
-									if err != nil {
-										log.Errorln(logTag, ":", err)
-										util.WriteBackError(w, "can't add search response to final response", http.StatusInternalServerError)
-										return
-									}
-									rsResponse = rsResponseWithSearchResponse
-									continue
-								}
-								if err1 != nil {
-									log.Errorln(logTag, ":", err1)
-									util.WriteBackError(w, "error while retriving hits: "+err1.Error(), http.StatusInternalServerError)
-									return
-								}
-								err := json.Unmarshal(hits, &rawHits)
-								if err != nil {
-									log.Errorln(logTag, ":", err)
-									util.WriteBackError(w, "error while parsing ES response to hits: "+err.Error(), http.StatusInternalServerError)
-									return
-								}
-								// extract category suggestions
-								if query.CategoryField != nil && *query.CategoryField != "" {
-									categories, dataType2, _, err2 := jsonparser.Get(value, "aggregations", *query.CategoryField, "buckets")
-									if err2 != nil {
-										log.Errorln(logTag, ":", err2)
-										util.WriteBackError(w, "error while retriving aggregations: "+err2.Error(), http.StatusInternalServerError)
-										return
-									}
-									if dataType2 != jsonparser.NotExist {
-										var buckets []es7.AggregationBucketKeyItem
-										err := json.Unmarshal(categories, &buckets)
-										if err != nil {
-											log.Errorln(logTag, ":", err)
-											util.WriteBackError(w, "error while parsing ES aggregations to suggestions: "+err.Error(), http.StatusInternalServerError)
-											return
-										}
-										for _, v := range buckets {
-											key, ok := v.Key.(string)
-											if ok {
-												count := int(v.DocCount)
-												suggestions = append(suggestions, SuggestionHIT{
-													Label:    valueAsString + " in " + key,
-													Value:    valueAsString,
-													Count:    &count,
-													Category: &key,
-												})
-											}
-										}
-									}
-								}
-
-								// extract index suggestions
-								suggestions = append(suggestions, getIndexSuggestions(suggestionsConfig, rawHits)...)
-								if query.Size != nil {
-									// fit suggestions to the max requested size
-									if len(suggestions) > *query.Size {
-										suggestions = suggestions[:*query.Size]
-									}
-								}
-							}
-						}
-					}
-				}
-				if isSuggestionRequest {
-					responseInByte, err := json.Marshal(suggestions)
-					if err != nil {
-						log.Errorln(logTag, ":", err)
-						util.WriteBackError(w, "error while parsing suggestions", http.StatusInternalServerError)
-						return
-					}
-					rsResponseWithSuggestions, err := jsonparser.Set(value, responseInByte, "hits", "hits")
-					if err != nil {
-						log.Errorln(logTag, ":", err)
-						util.WriteBackError(w, "can't add suggestions to final response", http.StatusInternalServerError)
-						return
-					}
-					rsResponseWithSearchResponse, err := jsonparser.Set(rsResponse, rsResponseWithSuggestions, queryID)
-					if err != nil {
-						log.Errorln(logTag, ":", err)
-						util.WriteBackError(w, "can't add search response to final response", http.StatusInternalServerError)
-						return
-					}
-					// Modify total suggestions value
-					rsResponseWithSearchResponse, err2 := jsonparser.Set(rsResponseWithSearchResponse, []byte(strconv.Itoa(len(suggestions))), queryID, "hits", "total", "value")
-					if err2 != nil {
-						log.Errorln(logTag, ":", err2)
-						util.WriteBackError(w, "can't apply total value for hits", http.StatusInternalServerError)
-						return
-					}
-					rsResponse = rsResponseWithSearchResponse
-				} else {
-					rsResponseWithSearchResponse, err := jsonparser.Set(rsResponse, value, queryID)
-					if err != nil {
-						log.Errorln(logTag, ":", err)
-						util.WriteBackError(w, "can't add search response to final response", http.StatusInternalServerError)
-						return
-					}
-					rsResponse = rsResponseWithSearchResponse
-				}
-				index++
-			})
 		}
 
 		indices, err := index.FromContext(req.Context())
@@ -294,4 +108,200 @@ func (r *QueryTranslate) validate() http.HandlerFunc {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(string(reqBody)))
 	}
+}
+
+func TransformESResponse(response []byte, rsAPIRequest *RSQuery) ([]byte, error) {
+	queryIds := GetQueryIds(*rsAPIRequest)
+
+	rsResponse := []byte(`{}`)
+
+	took, valueType1, _, err := jsonparser.Get(response, "took")
+	// ignore not exist error
+	if err != nil && valueType1 != jsonparser.NotExist {
+		log.Errorln(logTag, ":", err)
+		return nil, errors.New("can't parse took key from response")
+	}
+	// Set the `settings` key to response
+	rsResponseWithTook, err := jsonparser.Set(rsResponse, []byte(fmt.Sprintf(`{ "took": %s }`, string(took))), "settings")
+	if err != nil {
+		log.Errorln(logTag, ":", err)
+		return nil, errors.New("can't add settings key to response")
+	}
+	// Assign updated json to actual response
+	rsResponse = rsResponseWithTook
+
+	responseError, valueType2, _, err := jsonparser.Get(response, "error")
+	// ignore not exist error
+	if err != nil && valueType2 != jsonparser.NotExist {
+		log.Errorln(logTag, ":", err)
+		return nil, errors.New("can't parse error key from response")
+	} else if responseError != nil {
+		// Set the `error` key to response
+		rsResponseWithError, err := jsonparser.Set(rsResponse, responseError, "error")
+		if err != nil {
+			log.Errorln(logTag, ":", err)
+			return nil, errors.New("can't add error key to response")
+		}
+		// Assign updated json to actual response
+		rsResponse = rsResponseWithError
+	}
+
+	// Read `responses` value from the response body
+	responses, valueType3, _, err4 := jsonparser.Get(response, "responses")
+	// ignore not exist error
+	if err4 != nil && valueType3 != jsonparser.NotExist {
+		log.Errorln(logTag, ":", err4)
+		return nil, errors.New("can't parse responses key from response")
+	}
+
+	if responses != nil {
+		index := 0
+		var parsingError error
+		// Set `responses` by query ID
+		jsonparser.ArrayEach(responses, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+			queryID := queryIds[index]
+			var isSuggestionRequest bool
+			var suggestions = make([]SuggestionHIT, 0)
+			// parse suggestions if query is of type `suggestion`
+			for _, query := range rsAPIRequest.Query {
+				if *query.ID == queryID && query.Type == Suggestion {
+					isSuggestionRequest = true
+					// Index suggestions are not meant for empty query
+					if query.Value != nil {
+						valueAsString, ok := (*query.Value).(string)
+						if ok {
+							var normalizedDataFields = []string{}
+							normalizedFields := NormalizedDataFields(query.DataField, query.FieldWeights)
+							for _, dataField := range normalizedFields {
+								normalizedDataFields = append(normalizedDataFields, dataField.Field)
+							}
+							suggestionsConfig := SuggestionsConfig{
+								// Fields to extract suggestions
+								DataFields: normalizedDataFields,
+								// Query value
+								Value:                       valueAsString,
+								ShowDistinctSuggestions:     query.ShowDistinctSuggestions,
+								EnablePredictiveSuggestions: query.EnablePredictiveSuggestions,
+								MaxPredictedWords:           query.MaxPredictedWords,
+								EnableSynonyms:              query.EnableSynonyms,
+								ApplyStopwords:              query.ApplyStopwords,
+								Stopwords:                   query.Stopwords,
+								URLField:                    query.URLField,
+								CategoryField:               query.CategoryField,
+								HighlightField:              query.HighlightField,
+								HighlightConfig:             query.HighlightConfig,
+								Language:                    query.SearchLanguage,
+							}
+
+							var rawHits []ESDoc
+							hits, dataType, _, err1 := jsonparser.Get(value, "hits", "hits")
+							if dataType == jsonparser.NotExist {
+								// write raw response
+								rsResponseWithSearchResponse, err := jsonparser.Set(rsResponse, value, queryID)
+								if err != nil {
+									log.Errorln(logTag, ":", err)
+									parsingError = errors.New("can't add search response to final response")
+									return
+								}
+								rsResponse = rsResponseWithSearchResponse
+								continue
+							}
+							if err1 != nil {
+								log.Errorln(logTag, ":", err1)
+								parsingError = errors.New("error while retriving hits: " + err1.Error())
+								return
+
+							}
+							err := json.Unmarshal(hits, &rawHits)
+							if err != nil {
+								log.Errorln(logTag, ":", err)
+								parsingError = errors.New("error while parsing ES response to hits: " + err.Error())
+								return
+							}
+							// extract category suggestions
+							if query.CategoryField != nil && *query.CategoryField != "" {
+								categories, dataType2, _, err2 := jsonparser.Get(value, "aggregations", *query.CategoryField, "buckets")
+								if err2 != nil {
+									log.Errorln(logTag, ":", err2)
+									parsingError = errors.New("error while retriving aggregations: " + err2.Error())
+									return
+								}
+								if dataType2 != jsonparser.NotExist {
+									var buckets []es7.AggregationBucketKeyItem
+									err := json.Unmarshal(categories, &buckets)
+									if err != nil {
+										log.Errorln(logTag, ":", err)
+										parsingError = errors.New("error while parsing ES aggregations to suggestions: " + err.Error())
+										return
+									}
+									for _, v := range buckets {
+										key, ok := v.Key.(string)
+										if ok {
+											count := int(v.DocCount)
+											suggestions = append(suggestions, SuggestionHIT{
+												Label:    valueAsString + " in " + key,
+												Value:    valueAsString,
+												Count:    &count,
+												Category: &key,
+											})
+										}
+									}
+								}
+							}
+
+							// extract index suggestions
+							suggestions = append(suggestions, getIndexSuggestions(suggestionsConfig, rawHits)...)
+							if query.Size != nil {
+								// fit suggestions to the max requested size
+								if len(suggestions) > *query.Size {
+									suggestions = suggestions[:*query.Size]
+								}
+							}
+						}
+					}
+				}
+			}
+			if isSuggestionRequest {
+				responseInByte, err := json.Marshal(suggestions)
+				if err != nil {
+					log.Errorln(logTag, ":", err)
+					parsingError = errors.New("error while parsing suggestions:" + err.Error())
+					return
+				}
+				rsResponseWithSuggestions, err := jsonparser.Set(value, responseInByte, "hits", "hits")
+				if err != nil {
+					log.Errorln(logTag, ":", err)
+					parsingError = errors.New("can't add suggestions to final response" + err.Error())
+					return
+				}
+				rsResponseWithSearchResponse, err := jsonparser.Set(rsResponse, rsResponseWithSuggestions, queryID)
+				if err != nil {
+					log.Errorln(logTag, ":", err)
+					parsingError = errors.New("can't add search response to final response" + err.Error())
+					return
+				}
+				// Modify total suggestions value
+				rsResponseWithSearchResponse, err2 := jsonparser.Set(rsResponseWithSearchResponse, []byte(strconv.Itoa(len(suggestions))), queryID, "hits", "total", "value")
+				if err2 != nil {
+					log.Errorln(logTag, ":", err2)
+					parsingError = errors.New("can't apply total value for hits" + err2.Error())
+					return
+				}
+				rsResponse = rsResponseWithSearchResponse
+			} else {
+				rsResponseWithSearchResponse, err := jsonparser.Set(rsResponse, value, queryID)
+				if err != nil {
+					log.Errorln(logTag, ":", err)
+					parsingError = errors.New("can't add search response to final response" + err.Error())
+					return
+				}
+				rsResponse = rsResponseWithSearchResponse
+			}
+			index++
+		})
+		if parsingError != nil {
+			return nil, parsingError
+		}
+	}
+	return rsResponse, nil
 }
