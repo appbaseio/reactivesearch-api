1028121f3d2225c183dd178ea33c90cd8c4925f3
Deepjyoti Barman <deep.barman30@gmail.com>
2022-03-01 23:25:20 +0530

    Cleanup sensitive info from the ES logger print methods

--- util/es_logger.go
+++ util/es_logger.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"fmt"
+	"regexp"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -11,6 +12,7 @@ type WrapKitLoggerDebug struct {
 }
 
 func (logger WrapKitLoggerDebug) Printf(format string, vars ...interface{}) {
+	cleanSenstiveData(&vars)
 	log.Debugln("[ElasticSearch: Trace] => ", fmt.Sprintf(format, vars...))
 }
 
@@ -19,5 +21,31 @@ type WrapKitLoggerError struct {
 }
 
 func (logger WrapKitLoggerError) Printf(format string, vars ...interface{}) {
+	cleanSenstiveData(&vars)
 	log.Errorln("[ElasticSearch: Error] => ", fmt.Sprintf(format, vars...))
+}
+
+// cleanSenstiveData cleans credentials from the
+// variables, if any.
+func cleanSenstiveData(vars *[]interface{}) {
+	// Check if any var contains an URL, if it does, replace auth from the URL
+	for index, passedVar := range *vars {
+		// Cast the interface to a string
+		stringedVar, ok := passedVar.(string)
+		if !ok {
+			continue
+		}
+
+		// Check if URL
+		isURL, _ := regexp.MatchString(`^https?://(www.)?.+\..+$`, stringedVar)
+		if !isURL {
+			continue
+		}
+
+		// If it is an URL, clean it up
+		cleanerRe := regexp.MustCompile(`//.+:.+@`)
+		cleanedVar := cleanerRe.ReplaceAllString(stringedVar, "//***:***@")
+
+		(*vars)[index] = cleanedVar
+	}
 }
