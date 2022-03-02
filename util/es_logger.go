5cba4073dc10a4cdcc8cac77ff822889d2dc7f5a
Deepjyoti Barman <deep.barman30@gmail.com>
2022-03-02 13:40:15 +0530

    Add support to write deprecation warnings to debug

--- util/es_logger.go
+++ util/es_logger.go
@@ -2,6 +2,8 @@ package util
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -19,5 +21,25 @@ type WrapKitLoggerError struct {
 }
 
 func (logger WrapKitLoggerError) Printf(format string, vars ...interface{}) {
-	log.Errorln("[ElasticSearch: Error] => ", fmt.Sprintf(format, vars...))
+	// If the log contains deprecation, print it as debug and return
+	formattedStr := fmt.Sprintf(format, vars...)
+	if DebugDeprecationWarns(formattedStr) {
+		return
+	}
+
+	log.Errorln("[ElasticSearch: Error] => ", formattedStr)
+}
+
+// DebugDeprecationWarns converts all the error logs containing
+// deprecation warnings to debug logs so that it doesn't invoke sentry
+func DebugDeprecationWarns(formattedStr string) bool {
+	// Check if any of the vars contain `deprecation` in it.
+	isDeprecated, _ := regexp.MatchString(`.*deprecation.*`, strings.ToLower(formattedStr))
+
+	if isDeprecated {
+		log.Debug("[ElasticSearch: Trace] => ", formattedStr)
+		return true
+	}
+
+	return false
 }
