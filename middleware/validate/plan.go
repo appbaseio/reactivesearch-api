095b6bce2d5cd3449eddd1e5e812f9a069f82e49
bietkul <kuldepsaxena155@gmail.com>
2022-03-25 05:37:04 +0530

    feat: add support for feature name in plan validation error

--- middleware/validate/plan.go
+++ middleware/validate/plan.go
@@ -10,22 +10,26 @@ import (
 
 // Plan returns a middleware that validates the user's plan.
 // For e.g `validate.Plan([]util.Plan{util.ArcEnterprise}),` restricts the route to only appbase.io enterprise users.
-func Plan(validPlans []util.Plan, byPassValidation bool) middleware.Middleware {
+func Plan(validPlans []util.Plan, byPassValidation bool, featureName string) middleware.Middleware {
 	if util.ValidatePlans(validPlans, byPassValidation) {
 		return validPlan
 	}
-	return invalidPlan
+	return invalidPlanMiddleware(featureName)
 }
 
-// Throws the payment required error
-func invalidPlan(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		msg := "This feature is not available for the free plan users, please upgrade to a paid plan."
-		if util.GetTier() != nil {
-			msg = "This feature is not available for the " + util.GetTier().String() + " plan users, please upgrade to a higher plan."
+func invalidPlanMiddleware(featureName string) middleware.Middleware {
+	return func(h http.HandlerFunc) http.HandlerFunc {
+		feature := featureName
+		if feature == "" {
+			feature = "This feature"
 		}
-		telemetry.WriteBackErrorWithTelemetry(req, w, msg, http.StatusPaymentRequired)
-		return
+		return func(w http.ResponseWriter, req *http.Request) {
+			msg := feature + " is not available for the free plan users, please upgrade to a paid plan."
+			if util.GetTier() != nil {
+				msg = feature + " is not available for the " + util.GetTier().String() + " plan users, please upgrade to a higher plan."
+			}
+			telemetry.WriteBackErrorWithTelemetry(req, w, msg, http.StatusPaymentRequired)
+		}
 	}
 }
 
