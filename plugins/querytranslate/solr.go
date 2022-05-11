d217a75904ed7123df90e65b73647205cb148851
Deepjyoti Barman <deep.barman30@gmail.com>
2022-05-12 00:01:15 +0530

    Add support to validate keys based on what is supported map

--- plugins/querytranslate/solr.go
+++ plugins/querytranslate/solr.go
@@ -1,5 +1,13 @@
 package querytranslate
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+)
+
 // ConvertFunc will convert the passed value into a
 // string to be passed in Solr
 type ConvertFunc func(interface{}) string
@@ -22,5 +30,38 @@ func validateRSToSolrKey(rsBody *[]Query) error {
 	// is not present in RSToSolr map then we will have
 	// to throw an error to let the user know that the
 	// key is not supported.
+
+	// Parse the query to a map in order to check if keys
+	// are present.
+	for _, query := range *rsBody {
+		marshalledQuery, marshalErr := json.Marshal(query)
+		if marshalErr != nil {
+			errMsg := fmt.Sprint("error occurred while marshalling query to map to validate keys for solr conversion: ", marshalErr)
+			log.Errorln(logTag, ": ", errMsg)
+			return errors.New(errMsg)
+		}
+
+		var queryAsMap map[string]interface{}
+		unmarshallErr := json.Unmarshal(marshalledQuery, &queryAsMap)
+
+		if unmarshallErr != nil {
+			errMsg := fmt.Sprint("error while unmarshalling query to map to validate conversion of RS to Solr: ", unmarshallErr)
+			log.Errorln(logTag, ": ", errMsg)
+			return errors.New(errMsg)
+		}
+
+		for key, _ := range queryAsMap {
+			_, ok := RSToSolr[key]
+			if !ok {
+				// Key does not exist in RSToSolr but is passed
+				// We cannot allow this, so just raise an error.
+				errMsg := fmt.Sprintf("%s: key is not allowed since it is not supported by Solr", key)
+				log.Warnln(logTag, ": ", errMsg)
+				return errors.New(errMsg)
+			}
+		}
+
+	}
+
 	return nil
 }
