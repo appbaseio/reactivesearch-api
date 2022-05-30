6101ef756f54b5cfe191d6c30815bff66e28554a
Deepjyoti Barman <deep.barman30@gmail.com>
2022-05-30 23:37:11 +0530

    Add init method to delete records older than 7 days

--- plugins/nodes/dao_es7.go
+++ plugins/nodes/dao_es7.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/appbaseio/reactivesearch-api/util"
+	es7 "github.com/olivere/elastic/v7"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -35,5 +36,26 @@ func (es *elasticsearch) pingES7(ctx context.Context, machineID string) error {
 		log.Errorln(logTag, ": error indexing ping time:", err)
 		return err
 	}
+	return nil
+}
+
+// deleteOlderRecords will ping ElasticSearch with a delete_by_query request
+// where range will be used to delete records older than 7 days.
+//
+// We don't need to keep docs older than 7 days since we only need the node
+// count of the last 10 mins and the last 7 days.
+func (es *elasticsearch) deleteOlderRecords(ctx context.Context, machineID string) error {
+	// Get the minimum time allowed.
+	// We will get the current time - 7 days
+	minTime := time.Now().AddDate(0, 0, -7).Unix()
+
+	rangeQuery := es7.NewRangeQuery("ping_time").Lt(minTime)
+
+	_, err := util.GetClient7().DeleteByQuery().Index(es.indexName).Query(rangeQuery).Do(ctx)
+	if err != nil {
+		log.Errorln(logTag, ": error while deleting records older than 7 days, ", err)
+		return err
+	}
+
 	return nil
 }
