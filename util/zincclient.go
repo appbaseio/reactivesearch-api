5066e5d34974da0dad4aa7ca8112a00ed418d012
Deepjyoti Barman <deep.barman30@gmail.com>
2022-06-17 12:26:57 +0530

    Add init code to init a zinc client

---
+++ util/zincclient.go
@@ -0,0 +1,55 @@
+package util
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type ZincClient struct {
+	URL        string
+	Username   string
+	Password   string
+	AuthHeader string
+}
+
+var (
+	clientInit *sync.Once
+	zincClient *ZincClient
+)
+
+// GetZincData will return the zinc data from the
+// environment.
+//
+// The return will be three strings:
+// - URL
+// - username
+// - password
+func GetZincData() (string, string, string) {
+	zincURL := os.Getenv("ZINC_CLUSTER_URL")
+
+	if zincURL == "" {
+		log.Fatal("Error encountered: ", fmt.Errorf("ES_CLUSTER_URL must be set in the environment variables"))
+	}
+
+	username, password := "", ""
+
+	if strings.Contains(zincURL, "@") {
+		splitIndex := strings.LastIndex(zincURL, "@")
+		protocolWithCredentials := strings.Split(zincURL[0:splitIndex], "://")
+		credentials := protocolWithCredentials[1]
+		protocol := protocolWithCredentials[0]
+		host := zincURL[splitIndex+1:]
+
+		credentialSeparator := strings.Index(credentials, ":")
+		username = credentials[0:credentialSeparator]
+		password = credentials[credentialSeparator+1:]
+
+		zincURL = fmt.Sprintf("%s://%s", protocol, host)
+
+	}
+	return zincURL, username, password
+}
