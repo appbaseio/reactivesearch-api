b25509957975699d7529b7316b557f9ee33a59b5
Deepjyoti Barman <deep.barman30@gmail.com>
2022-07-12 09:54:28 +0530

    Add support to properly print the version from RS

--- main.go
+++ main.go
@@ -121,6 +121,7 @@ func init() {
 	flag.StringVar(&address, "addr", "0.0.0.0", "Address to serve on")
 	flag.BoolVar(&disableHealthCheck, "disable-health-check", false, "Set as `true` to disable health check")
 	flag.BoolVar(&showVersion, "version", false, "show the version of ReactiveSearch")
+
 	// env port for deployments like heroku where port is dynamically assigned
 	envPort := os.Getenv("PORT")
 	defaultPort := 8000
@@ -129,13 +130,24 @@ func init() {
 		defaultPort = portValue
 	}
 
-	fmt.Println("=> port used", defaultPort)
 	flag.IntVar(&port, "port", defaultPort, "Port number")
 	flag.StringVar(&pluginDir, "pluginDir", "build/plugins", "Directory containing the compiled plugins")
 	flag.BoolVar(&https, "https", false, "Starts a https server instead of a http server if true")
 	flag.BoolVar(&cpuprofile, "cpuprofile", false, "write cpu profile to `file`")
 	flag.BoolVar(&memprofile, "memprofile", false, "write mem profile to `file`")
 	flag.Parse()
+
+	// If showVersion is passed, show the version and do
+	// nothing.
+	util.Version = Version
+
+	if showVersion {
+		fmt.Println(fmt.Sprintf("ReactiveSearch v%s", util.Version))
+		os.Exit(0)
+	}
+
+	fmt.Println("=> port used", defaultPort)
+
 	// Set telemetry based on the user input
 	// Runtime flag gets the highest priority
 	telemetryEnvVar := os.Getenv("ENABLE_TELEMETRY")
@@ -182,15 +194,6 @@ func init() {
 }
 
 func main() {
-	// If showVersion is passed, show the version and do
-	// nothing.
-	util.Version = Version
-
-	if showVersion {
-		log.Info("ReactiveSearch ", util.Version)
-		return
-	}
-
 	// add cpu profilling
 	if cpuprofile {
 		defer profile.Start().Stop()
