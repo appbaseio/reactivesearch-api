a2679c0492365903bc6e84b14fb975312fcecafc
Deepjyoti Barman <deep.barman30@gmail.com>
2022-07-13 13:53:31 +0530

    Add comments about how injection is happening

--- plugins/querytranslate/schema.go
+++ plugins/querytranslate/schema.go
@@ -3,88 +3,17 @@ package querytranslate
 import (
 	"encoding/json"
 	"fmt"
-	"reflect"
-	"regexp"
-	"strings"
 )
 
-// AddAdditionalFields will add new fields in the struct tag
-// according to the ID of the struct type.
+// injectExtrasToSchema will inject extra fields in the schema.
 //
-// As of now, this function will inject the following fields
-// if available for the passed ID:
+// This method will modify the `properties.query.items.properties`
+// and `properties.settings.properties` to inject the following fields:
 // - markdownDescription
-func AddAdditionalFields(typePassed reflect.Type) []reflect.StructField {
-	structFieldsToReturn := make([]reflect.StructField, 0)
-
-	for index := 0; index < typePassed.NumField(); index++ {
-		// Get the field
-		fieldToWorkOn := typePassed.Field(index)
-
-		// Extract the struct tag of the field,
-		// Get the jsonschema_extras field and inject the markdownDescription.
-		// Build a new struct tag field and add it to the current struct
-		// field and finally add it to the final response array.
-		tagOfField := fieldToWorkOn.Tag
-
-		// Extract the ID of the field
-		// The `json` value will be something like "id,omitempty".
-		//
-		// So, we can get it, split it by comma (,) and use the first index element
-		// as the ID.
-		jsonAsArr := regexp.MustCompile(`,[[:space:]]?`).Split(tagOfField.Get("json"), -1)
-
-		IDToUse := ""
-
-		if len(jsonAsArr) < 1 {
-			// Use the lowercase'd name value as a fallback
-			IDToUse = strings.ToLower(fieldToWorkOn.Name)
-		} else {
-			IDToUse = jsonAsArr[0]
-		}
-
-		updatedExtras := injectMarkdownDescription(tagOfField.Get("jsonschema_extras"), IDToUse)
-
-		re := regexp.MustCompile(`jsonschema_extras:".*?"`)
-		updatedTag := re.ReplaceAllString(string(tagOfField), fmt.Sprintf(`jsonschema_extras:"%s"`, updatedExtras))
-
-		fieldToWorkOn.Tag = reflect.StructTag(updatedTag)
-
-		structFieldsToReturn = append(structFieldsToReturn, fieldToWorkOn)
-	}
-
-	return structFieldsToReturn
-}
-
-// injectMarkdownDescription will inject the markdown description
-// field to the jsonschema_extras field passed and return the
-// modified string
-func injectMarkdownDescription(extras string, ID string) string {
-	// If the field is already present, no need to modify
-	// the string.
-	if strings.Contains(extras, "markdownDescription") {
-		return extras
-	}
-
-	// Split the extras string based on comma
-	// TODO: Remove whitespace after commas from the string
-
-	splittedExtras := strings.Split(extras, ",")
-
-	// Try to get the markdownDescription for the passed ID.
-	mdDesc, isMdPresent := MARKDOWN_DESCRIPTIONS[ID]
-
-	// If no description is present for the passed ID
-	if !isMdPresent {
-		return extras
-	}
-
-	// Inject markdownDescription
-	splittedExtras = append(splittedExtras, fmt.Sprintf("markdownDescription=%s", mdDesc))
-
-	return strings.Join(splittedExtras, ",")
-}
-
+// - playgroundURL
+//
+// Note that if the field value is not present based on the ID, it will
+// not be added to the schema
 func injectExtrasToSchema(schemaMarshalled []byte) ([]byte, error) {
 	schemaAsMap := make(map[string]interface{})
 	unmarshalErr := json.Unmarshal(schemaMarshalled, &schemaAsMap)
