a17793f1737dba99c47f39093fc72fdc887fc9f6
Deepjyoti Barman <deep.barman30@gmail.com>
2022-09-29 18:50:32 +0530

    Add a fix for ES_URL to be set before client init

--- main.go
+++ main.go
@@ -453,7 +453,12 @@ func main() {
 
 	// ES client instantiation
 	// ES v7 and v6 clients
-	util.NewClient()
+
+	// If SLS is disabled, create the client as is.
+	if util.IsSLSDisabled() {
+		util.NewClient()
+	}
+
 	util.NewZincClient()
 	util.SetDefaultIndexTemplate()
 	util.SetSystemIndexTemplate()
@@ -507,6 +512,13 @@ func main() {
 			log.Fatal("error loading plugins: ", errPipelinesPlugin)
 		}
 	}
+
+	// If SLS is enabled, we need to create the ES client after pipelines
+	// is initialized.
+	if util.IsSLSEnabled() {
+		util.NewClient()
+	}
+
 	for _, pluginPath := range pluginsByPath {
 		plugin, err1 := LoadPluginFromFile(mainRouter, pluginPath)
 		if err1 != nil {
