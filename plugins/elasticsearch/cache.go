ce729edbd98898f1df199e9fca9d68203a79169c
Deepjyoti Barman <deep.barman30@gmail.com>
2022-12-07 10:04:02 +0530

    Modify the cache method to store indexes efficiently

--- plugins/elasticsearch/cache.go
+++ plugins/elasticsearch/cache.go
@@ -8,5 +8,12 @@ var tenantToIndexMap map[string][]string
 
 // SetIndexesToCache will set the index into the cache map
 func SetIndexesToCache(tenantID string, index string) {
+	// Check if the entry for the tenantID already exists,
+	// if it doesn't exist, then create it.
+	_, keyExists := tenantToIndexMap[tenantID]
+	if !keyExists {
+		tenantToIndexMap[tenantID] = make([]string, 0)
+	}
 
+	tenantToIndexMap[tenantID] = append(tenantToIndexMap[tenantID], index)
 }
