28cc975ee109b6610afd4e17d9d43389b1dd5478
Deepjyoti Barman <deep.barman30@gmail.com>
2022-12-13 19:52:11 +0530

    Fix issue with record usage crashing

--- util/record_usage.go
+++ util/record_usage.go
@@ -10,7 +10,7 @@ import (
 )
 
 // Domain to usage map
-var totalUsage map[string]int
+var totalUsage map[string]int = make(map[string]int)
 
 func addToUsage(domain string, usage int) {
 	if _, ok := totalUsage[domain]; ok {
