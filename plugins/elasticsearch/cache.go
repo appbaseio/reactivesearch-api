f1fd5b0bc8bc783f918eb0226b97fc3a92db7be4
Deepjyoti Barman <deep.barman30@gmail.com>
2022-12-16 10:56:23 +0530

    Add support to optimize storage of cached indices

--- plugins/elasticsearch/cache.go
+++ plugins/elasticsearch/cache.go
@@ -6,7 +6,7 @@ import "sort"
 //
 // The name of the index will be stored without the `tenant_id`
 // appended in it.
-var tenantToIndexMap map[string][]string = make(map[string][]string)
+var tenantToIndexMap map[string]map[string]int = make(map[string]map[string]int)
 
 // SetIndexToCache will set the index into the cache map
 func SetIndexToCache(tenantID string, index string) {
@@ -14,39 +14,16 @@ func SetIndexToCache(tenantID string, index string) {
 	// if it doesn't exist, then create it.
 	_, keyExists := tenantToIndexMap[tenantID]
 	if !keyExists {
-		tenantToIndexMap[tenantID] = make([]string, 0)
+		tenantToIndexMap[tenantID] = make(map[string]int)
 	}
 
-	tenantToIndexMap[tenantID] = append(tenantToIndexMap[tenantID], index)
-}
-
-// GetIndexLocFromCache will try to get the index by using
-// the passed tenantId and the index name
-func GetIndexLocFromCache(tenantID string, index string) *int {
-	indicesList, exists := tenantToIndexMap[tenantID]
-	if !exists {
-		return nil
-	}
-
-	for indexPosition, indexName := range indicesList {
-		if indexName == index {
-			return &indexPosition
-		}
-	}
-
-	return nil
+	tenantToIndexMap[tenantID][index] = 1
 }
 
 // DeleteIndexFromCache will remove the index from the cache
 // map
 func DeleteIndexFromCache(tenantID string, index string) bool {
-	location := GetIndexLocFromCache(tenantID, index)
-	if location == nil {
-		return false
-	}
-
-	tenantToIndexMap[tenantID] = append(tenantToIndexMap[tenantID][:*location], tenantToIndexMap[tenantID][*location+1:]...)
-
+	delete(tenantToIndexMap[tenantID], index)
 	return true
 }
 
@@ -61,8 +38,13 @@ func GetCachedIndices(tenantID string) []string {
 		return make([]string, 0)
 	}
 
-	sort.Slice(cachedIndices, func(i, j int) bool {
-		return len(cachedIndices[i]) > len(cachedIndices[j])
+	indicesArr := make([]string, 0)
+	for cachedIndex := range cachedIndices {
+		indicesArr = append(indicesArr, cachedIndex)
+	}
+
+	sort.Slice(indicesArr, func(i, j int) bool {
+		return len(indicesArr[i]) > len(indicesArr[j])
 	})
-	return cachedIndices
+	return indicesArr
 }
